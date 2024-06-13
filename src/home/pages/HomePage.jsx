faf1f8ae799eee81bc9306466e533dc137d79c3b
mercedeslop <jenifersasa2004@gmail.com>
2024-06-13 17:29:44 -0500

    Modificaciones en la sección Home(Product_section)

--- src/home/pages/HomePage.jsx
+++ src/home/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/Footer"
 import Navbar from "../../components/Navbar"
 import Hero_home from "./Hero_home"
+import Producto_section from "./Product_section"
 
 const HomePage = () => {
     return (
         <div>
             <Navbar />
             <Hero_home />
+            <Producto_section />
             <Footer />
         </div>
     )
---
+++ src/home/pages/Product_section.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+function Producto_section(){
+    return(
+       
+		<div class="product-section">
+			<div class="container">
+				<div class="row">
+
+					
+					<div class="col-md-12 col-lg-3 mb-5 mb-lg-0">
+						<h2 class="mb-4 section-title">Crafted with excellent material.</h2>
+						<p class="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. </p>
+						<p><a href="shop.html" class="btn">Explore</a></p>
+					</div> 
+					
+
+				
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<a class="product-item" href="cart.html">
+							<img src="image/product-1.png" class="img-fluid product-thumbnail"></img>
+							<h3 class="product-title">Nordic Chair</h3>
+							<strong class="product-price">$50.00</strong>
+
+							<span class="icon-cross">
+								<img src="image/cross.svg" class="img-fluid"></img>
+							</span>
+						</a>
+					</div> 
+					
+
+					
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<a class="product-item" href="cart.html">
+							<img src="image/product-2.png" class="img-fluid product-thumbnail"></img>
+							<h3 class="product-title">Kruzo Aero Chair</h3>
+							<strong class="product-price">$78.00</strong>
+
+							<span class="icon-cross">
+								<img src="image/cross.svg" class="img-fluid"></img>
+							</span>
+						</a>
+					</div>
+					
+
+			
+					<div class="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
+						<a class="product-item" href="cart.html">
+							<img src="image/product-3.png" class="img-fluid product-thumbnail"></img>
+							<h3 class="product-title">Ergonomic Chair</h3>
+							<strong class="product-price">$43.00</strong>
+
+							<span class="icon-cross">
+								<img src="image/cross.svg" class="img-fluid"></img>
+							</span>
+						</a>
+					</div>
+		
+
+				</div>
+			</div>
+		</div>
+		
+    );
+}
+
+export default Producto_section;
